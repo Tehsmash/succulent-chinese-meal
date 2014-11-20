b8854e7c8b88bbc829bcb545fc4ead945c77b877
Rob <robert.cresswell@outlook.com>
2014-11-20 16:50:54 +0000

    Crappy compute controller changes

--- public/app/compute/compute.js
+++ public/app/compute/compute.js
@@ -3,15 +3,44 @@
 var compute = angular.module('horizon.compute', ['ngRoute'])
 
 compute.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/compute', {
-      templateUrl: 'compute/compute.html',
-      controller: 'ComputeController'
-    });
+  .when('/compute', {
+    templateUrl: 'compute/compute.html',
+    controller: 'ComputeController'
+  });
 }])
 
-compute.controller('ComputeController', function($scope) {
-  
+compute.controller('ComputeController', function($scope, instanceFactory) {
 
+  $scope.instances = instanceFactory.getInstances();
 
+  // Create a instance from the instance object
+  $scope.createInstance = function() {
+    var uuid = Math.random().toString(36).substring(7);
+    var instance = {
+      uuid: uuid, 
+      name: $scope.instance.name || uuid,
+    };
+    instanceFactory.setInstance(instance);
+    $scope.newInstance();
+  }
+
+  $scope.clearFilter = function(){
+    $scope.filter = null;
+  }
+
+  // Delete a instance
+  $scope.deleteInstance = function(index) {
+    instanceFactory.deleteInstance(index);
+  }
+
+  // Initialise empty instance object. 
+  $scope.newInstance = function() {
+    $scope.instance = {
+      uuid: null,
+      name: null,
+      adminState: true
+    };  
+  }
+
+  $scope.newInstance();
 });
